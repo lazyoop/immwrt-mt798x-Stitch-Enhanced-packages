lazyoop/immwrt-mt798x-Stitch-Enhanced-packages

commit 03951c573074553a8dc84e3f343b8e4f9d4761bb

mwan3: lock critical mwan3rtmon section
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

net/mwan3/files/usr/sbin/mwan3rtmon
@@ -23,11 +23,16 @@ main() {
 	config_get rtmon_interval globals rtmon_interval '5'
 
 	sleep 3
-	while mwan3_rtmon_ipv4 || mwan3_rtmon_ipv6; do
+	while true; do
+		mwan3_lock
+		[ mwan3_remon_ipv4 = "1" ] || \
+		[ mwan3_rtmon_ipv6 = "1" ] && break
+		mwan3_unlock
 		[ "$rtmon_interval" = "0" ] && break
 		sleep "$rtmon_interval" &
 		wait
 	done
+	mwan3_unlock
 }
 
 main "$@"

commit 907b8b1915f99f7b2a7aa6bf100aacec60a64059

mwan3: fix logical/typo bug in mwan3rtmon
This bug was introduced since 03951c573074553a8dc84e3f343b8e4f9d4761bb
mwan3_remon_ipv4 and mwan3_remon_ipv6 is command to run not a variable
I add some comments on them hopefully people will notice it

Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

net/mwan3/Makefile
@@ -8,7 +8,7 @@
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=mwan3
-PKG_VERSION:=2.7.11
+PKG_VERSION:=2.7.12
 PKG_RELEASE:=1
 PKG_MAINTAINER:=Florian Eckert <fe@dev.tdt.de>
 PKG_LICENSE:=GPLv2

net/mwan3/files/lib/mwan3/mwan3.sh
@@ -32,6 +32,8 @@ MM_BLACKHOLE=""
 MMX_UNREACHABLE=""
 MM_UNREACHABLE=""
 
+# return true(=0) if has any mwan3 interface enabled
+# otherwise return false
 mwan3_rtmon_ipv4()
 {
 	local tid=1
@@ -61,6 +63,8 @@ mwan3_rtmon_ipv4()
 	return $ret
 }
 
+# return true(=0) if has any mwan3 interface enabled
+# otherwise return false
 mwan3_rtmon_ipv6()
 {
 	local tid=1

net/mwan3/files/usr/sbin/mwan3rtmon
@@ -25,14 +25,14 @@ main() {
 	sleep 3
 	while true; do
 		mwan3_lock "service" "mwan3rtmon"
-		[ mwan3_remon_ipv4 = "1" ] || \
-		[ mwan3_rtmon_ipv6 = "1" ] && break
+		mwan3_rtmon_ipv4 || mwan3_rtmon_ipv6
+		ret=$?
 		mwan3_unlock "service" "mwan3rtmon"
+		[ "$ret" = "0" ] || break
 		[ "$rtmon_interval" = "0" ] && break
 		sleep "$rtmon_interval" &
 		wait
 	done
-	mwan3_unlock "service" "mwan3rtmon"
 }
 
 main "$@"